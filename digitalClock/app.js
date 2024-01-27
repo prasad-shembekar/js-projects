84623b833c99299c2acbaf36dc6ef47bef071187
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-27 13:55:47 +0000

    Update

--- digitalClock/app.js
+++ digitalClock/app.js
@@ -1,8 +1,8 @@
 const clock = document.getElementById('clock');
 
-let date = new Date();
-console.log(date.toLocaleString());
 
 setInterval(()=>{
-    
+    let date = new Date();
+    // console.log(date.toLocaleString());
+    clock.innerHTML = date.toLocaleTimeString()
 },1000)

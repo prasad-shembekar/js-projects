67cd0db4dd6291769406c75f86a5b2d17e85ccd1
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-27 13:27:13 +0000

    Update

---
+++ bmiCalculator/app.js
@@ -0,0 +1,18 @@
+const form  = document.querySelector('form');
+
+// const weight = parseInt(document.querySelector("#weight").value);
+// if we take value from here it will provide you empty values. 
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector("#height").value);
+    const weight = parseInt(document.querySelector("#weight").value);
+    const results = document.querySelector("#results");
+    
+    if(height === '' || height < 0 || isNaN(height)){
+        results.innerHTML = "Please give valid height"
+    }
+
+    
+});

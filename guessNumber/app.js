b329f6335094cb2ab3e451e97690b500dbec7e57
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-27 14:17:25 +0000

    Update

--- guessNumber/app.js
+++ guessNumber/app.js
@@ -2,4 +2,6 @@ const randomNumber = console.log(parseInt(Math.random()*100+1));
 
 const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
+const guesSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
 

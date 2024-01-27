a23134908e863259909ee300b26b066d4dc30488
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-01-27 07:24:25 +0000

    Update

--- colorSwitcher/app.js
+++ colorSwitcher/app.js
@@ -7,5 +7,14 @@ buttons.forEach(function(button){
     button.addEventListener("click",function(e){
         console.log(e);
         console.log(e.target);
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id;
+        }else if(e.target.id === 'pink'){
+            body.style.backgroundColor = e.target.id;
+        }else if(e.target.id === 'orange'){
+            body.style.backgroundColor = e.target.id;
+        }else if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
     })
 })

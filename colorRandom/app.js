af2279c9adcf9b019b3c3a4b6d90e79feeded52f
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-02-20 11:07:49 +0000

    Update

--- colorRandom/app.js
+++ colorRandom/app.js
@@ -1,23 +1,31 @@
-// generate random color hex
+//generate a random color
 
-const randomColor = function(){
-    const hex = '0123456789ABCDEF'
+const randomColor = function () {
+    const hex = '0123456789ABCDEF';
     let color = '#';
-
-    for(let i= 0; i< 6;i++){
-        color += hex[Math.floor(Math.random()*16)]
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * 16)];
     }
     return color;
-}
-
-const startChangingColor = function(){
-    document.body.style.backgroundColor = randomColor();
-};
-
-const stopChangingColor = function(){
-
-}
-
-document.querySelector('#start').addEventListener('click',startChangingColor);
-document.querySelector('#stop').addEventListener('click',stopChangingColor);
-
+  };
+  
+  let intervalId;
+  const startChangingColor = function () {
+    if (!intervalId) {
+      intervalId = setInterval(changeBgColor, 1000);
+    }
+  
+    function changeBgColor() {
+      document.body.style.backgroundColor = randomColor();
+    }
+  };
+  const stopChangingColor = function () {
+    clearInterval(intervalId);
+    intervalId = null;
+  };
+  
+  document.querySelector('#start').addEventListener('click', startChangingColor);
+  
+  document.querySelector('#stop').addEventListener('click', stopChangingColor);
+  
+  

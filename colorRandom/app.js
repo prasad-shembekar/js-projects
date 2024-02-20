a89f005d19c9ff630744f7e6d1c6896b2d60e1b9
Prasad Shembekar <89524884+prasad-shembekar@users.noreply.github.com>
2024-02-20 10:53:55 +0000

    Update

--- colorRandom/app.js
+++ colorRandom/app.js
@@ -5,6 +5,9 @@ const randomColor = function(){
     let color = '#';
 
     for(let i= 0; i< 6;i++){
-        
+        color += hex[Math.floor(Math.random()*16)]
     }
+    return color;
 }
+
+console.log(randomColor());
